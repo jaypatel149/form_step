1159a7f9be2cabdf9caa05b4c7abb0202c385c22
jay.st <jppatel.st@gmail.com>
2023-07-19 12:47:15 +0530

    created form step

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Form from './components/Form'
+import FormStep5 from './components/FormSptep5'
+import FormStep2 from './components/FormStep2'
+import FormStep3 from './components/FormStep3'
+import FormStep4 from './components/FormStep4'
+
+const App = () => {
+
+  
+
+
+  return (
+    <div>
+      <Form/>
+      <FormStep2/>
+      <FormStep3/>
+      <FormStep4/>
+      <FormStep5/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/FormSptep5.jsx
@@ -0,0 +1,57 @@
+// import React from 'react'
+
+
+
+
+const FormStep5 = () => {
+    return (
+      <div className="container">
+         <form>
+          <div className="mb-3">
+            <label htmlFor="firstname" className="form-label">
+              FirstName
+            </label>
+            <input
+              type="text"
+              name="firstName"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="lastname" className="form-label">
+              LastName
+            </label>
+            <input
+              type="text"
+              name="lastName"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
+      </div>
+    )
+  }
+  
+  export default FormStep5
+  
